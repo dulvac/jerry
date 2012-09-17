5ca8229c3cf1d4de0d7989bc57348d36a696ea6d
Andrei Dulvac <adulvac@adobe.com>
2012-09-17 19:54:39 +0300

    Fixed startup bash script

--- bin/jerry_server.sh
+++ bin/jerry_server.sh
@@ -1,1 +1,5 @@
-java -cp "conf/:target/jserver-1.0.jar:target/dependency/*" com.dulvac.jerry.Server
+#!/bin/bash
+THIS_DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+pushd "${THIS_DIR}/../"
+java -cp "conf/:target/jserver-1.0.jar:target/dependency/*" com.dulvac.jerry.Server $@
+popd
