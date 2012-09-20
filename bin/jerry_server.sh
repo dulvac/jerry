8d7a647190e5d6654ed53f94d9d654bffe2ca3f9
Andrei Dulvac <adulvac@adobe.com>
2012-09-20 13:27:53 +0300

    Changed project name from jserver to jerry

--- bin/jerry_server.sh
+++ bin/jerry_server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 THIS_DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 pushd "${THIS_DIR}/../"
-java -cp "conf/:target/jserver-1.0.jar:target/dependency/*" com.dulvac.jerry.Server $@
+java -cp "conf/:target/jerry-1.0.jar:target/dependency/*" com.dulvac.jerry.Server $@
 popd
