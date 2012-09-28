f14e4735f76152113b3603de995ee7a12f7e09b6
Andrei Dulvac <andrei.dulvac@gmail.com>
2012-09-28 14:24:54 +0300

    Added test for loading configuration

--- src/test/java/com/dulvac/jerry/JerryServerTest.java
+++ src/test/java/com/dulvac/jerry/JerryServerTest.java
@@ -1,47 +1,67 @@
 package com.dulvac.jerry;
 
-import java.util.*;
+import org.apache.commons.configuration.HierarchicalConfiguration;
+import org.apache.http.params.CoreConnectionPNames;
 import org.testng.Assert;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.net.BindException;
 
 public class JerryServerTest {
 
-    private Collection collection;
+  @BeforeClass
+  public void oneTimeSetUp() {
+    // one-time initialization code
+  }
 
-    @BeforeClass
-    public void oneTimeSetUp() {
-        // one-time initialization code
-    	System.out.println("@BeforeClass - oneTimeSetUp");
+  @AfterClass
+  public void oneTimeTearDown() {
+    // one-time cleanup code
+  }
+
+  @BeforeMethod
+  public void setUp() {
+  }
+
+  @AfterMethod
+  public void tearDown() {
+  }
+
+  @Test
+  public void testLoadConfiguration() {
+    JerryServer js = new JerryServer();
+    // create listeners configuration
+    HierarchicalConfiguration config = new HierarchicalConfiguration();
+    config.addProperty("listeners.listener.filesRoot", "/some/path");
+    config.addProperty("jerry.listeners.listener.http.connection_timeout", "42");
+
+    int port = 18090;
+    config.addProperty("listeners.listener.port", "8080");
+    for (int i = 0; i < 10; i++) {
+      try {
+        // increment port number
+        js.loadConfiguration(config);
+        break;
+      } catch (BindException e) {
+        System.out.println("Couldn't start server on " + port + ". Trying " + port+1);
+        // increment port number
+        config.setProperty("listeners.listener.port", Integer.toString(++port));
+        continue;
+      } catch (IOException e) {
+        Assert.fail("Error loading configuration: ", e);
+      }
     }
 
-    @AfterClass
-    public void oneTimeTearDown() {
-        // one-time cleanup code
-    	System.out.println("@AfterClass - oneTimeTearDown");
-    }
-
-    @BeforeMethod
-    public void setUp() {
-        collection = new ArrayList();
-        System.out.println("@BeforeMethod - setUp");
-    }
-
-    @AfterMethod
-    public void tearDown() {
-        collection.clear();
-        System.out.println("@AfterMethod - tearDown");
-    }
-
-    @Test
-    public void testEmptyCollection() {
-        Assert.assertEquals(collection.isEmpty(),true);
-        System.out.println("@Test - testEmptyCollection");
-    }
-
-    @Test
-    public void testOneItemCollection() {
-        collection.add("itemA");
-        Assert.assertEquals(collection.size(),1);
-        System.out.println("@Test - testOneItemCollection");
-    }
+    Assert.assertTrue((null != js.requestListeners) && (js.requestListeners.size() == 1));
+    RequestListener rl = js.requestListeners.get(0);
+    Assert.assertEquals(rl.getListenPort(), port);
+    Assert.assertEquals(rl.getHttpParams().getIntParameter(CoreConnectionPNames.CONNECTION_TIMEOUT, -1), 42);
+    Assert.assertEquals(rl.getHttpParams().getIntParameter(CoreConnectionPNames.SOCKET_BUFFER_SIZE, -1),
+                        RequestListenerConfiguration.HTTP_SOCKET_BUFFER_SIZE);
+  }
 }
